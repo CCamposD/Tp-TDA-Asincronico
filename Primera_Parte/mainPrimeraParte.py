fe459d12e7c7b1f5b27733bc4b72a487c1194593
Campos D. Camilo <163214500+CCamposD@users.noreply.github.com>
2025-01-14 00:31:55 -0300

    Print faltante para la revisión
    Se agrega print en terminal para la Primera y Segunda parte

--- Primera_Parte/mainPrimeraParte.py
+++ Primera_Parte/mainPrimeraParte.py
@@ -1,3 +1,23 @@
+import sys
+
+def leer_monedas_desde_archivo(nombre_archivo):
+    try:
+        with open(nombre_archivo, 'r') as archivo:
+            
+            contenido = archivo.readline().strip()
+
+            # Separar por punto y coma
+            valores_lista = contenido.split(";")
+
+            # Convertir a lista de enteros
+            monedas = [int(valor) for valor in valores_lista]
+
+            return monedas
+
+    except FileNotFoundError:
+        print('No se encontró el archivo.')
+        sys.exit(1)
+
 def primer_moneda_mayor_valor(monedas):
     return monedas[0] > monedas[-1]
 
@@ -32,6 +52,11 @@ def elegir_moneda_y_actualizar(puntaje, monedas, turno_sophia):
 
         moneda, monedas_actualizadas = mateo_elige(monedas)
     
+    if turno_sophia:
+        print(f'Sophia elige la moneda: {moneda}')
+    else:
+        print(f'Mateo elige la moneda: {moneda}')
+    
     puntaje_actualizado = actualizar_puntaje(puntaje, moneda)
 
     return puntaje_actualizado, monedas_actualizadas
@@ -58,3 +83,21 @@ def juego_monedas(monedas):
         turno_sophia = not turno_sophia
 
     return puntaje_sophia, puntaje_mateo
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Uso: python mainPrimeraParte.py nombre_archivo')
+        sys.exit(1)
+
+    nombre_archivo = sys.argv[1]
+    monedas = leer_monedas_desde_archivo(nombre_archivo)
+
+    puntaje_sophia, puntaje_mateo = juego_monedas(monedas)
+
+    if puntaje_mateo < puntaje_sophia:
+        print('Gana Sophia')
+    elif puntaje_mateo > puntaje_sophia:
+        print('Gana Mateo')
+    else:
+        print('Empate')
--- Segunda_Parte/mainSegundaParte.py
+++ Segunda_Parte/mainSegundaParte.py
@@ -1,3 +1,24 @@
+import sys
+
+def leer_monedas_desde_archivo(nombre_archivo):
+    try:
+        with open(nombre_archivo, 'r') as archivo:
+            
+            contenido = archivo.readline().strip()
+
+            # Separar por punto y coma
+            valores_lista = contenido.split(";")
+
+            # Convertir a lista de enteros
+            monedas = [int(valor) for valor in valores_lista]
+
+            return monedas
+
+    except FileNotFoundError:
+        print('No se encontró el archivo.')
+        sys.exit(1)
+
+
 # Estrategia de Mateo
 
 def mateo_elige(monedas):
@@ -118,6 +139,11 @@ def elegir_moneda_y_actualizar(puntaje, monedas, turno_sophia):
         
     puntaje_actualizado = actualizar_puntaje(puntaje, moneda)
 
+    if turno_sophia:
+        print(f'Sophia elige la moneda: {moneda}')
+    else:
+        print(f'Mateo elige la moneda: {moneda}')
+    
     return puntaje_actualizado, monedas_actualizadas
 
 def jugar_turno(puntaje, monedas, turno_sophia):
@@ -148,3 +174,21 @@ def juego_monedas(monedas):
         turno_sophia = not turno_sophia
 
     return puntaje_sophia, puntaje_mateo
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Uso: python mainPrimeraParte.py nombre_archivo')
+        sys.exit(1)
+
+    nombre_archivo = sys.argv[1]
+    monedas = leer_monedas_desde_archivo(nombre_archivo)
+
+    puntaje_sophia, puntaje_mateo = juego_monedas(monedas)
+
+    if puntaje_mateo < puntaje_sophia:
+        print('Gana Sophia')
+    elif puntaje_mateo > puntaje_sophia:
+        print('Gana Mateo')
+    else:
+        print('Empate')
